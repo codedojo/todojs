47d2acc181ede3ffc998c57427b01d21bafa209e
Oleg Polyakov <olegpolyako@outlook.com>
2017-09-08 15:13:33 +0300

    Start

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const model = new Model();
-const controller = new Controller();
-const view = new View(model, controller);
+const presenter = new Presenter();
+const view = new View(model);
 
 controller.initialize(model, view);
